080cac26a2aa2cfa2da89b798f41e9d80b003579
BryanLeite-dev <bryan.leite@etc.sp.gov.br>
2021-05-24 12:14:45 -0300

    Inclusão do Projeto

---
+++ Assets/Script/Movimentação.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentação : MonoBehaviour
+{ 
+    public Vector3 scaleChange;
+    public Vector3 positionChange;
+    public Vector3 rotateChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += scaleChange;
+        transform.position += positionChange;
+        transform.Rotate(rotateChange);
+    }
+}
